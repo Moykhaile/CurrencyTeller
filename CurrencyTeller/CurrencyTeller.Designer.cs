873e17025517a536d7d817fef093f2a365328310
Gabriel R.R <gabriel1706.com@gmail.com>
2020-05-14 04:54:54 -0300

    Update 1.0
    New UI, improved functionalities and
    faster performance.

--- CurrencyTeller/CurrencyTeller.Designer.cs
+++ CurrencyTeller/CurrencyTeller.Designer.cs
@@ -28,18 +28,23 @@ namespace CurrencyTracker
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			this.DollarLabel = new System.Windows.Forms.Label();
-			this.EuroLabel = new System.Windows.Forms.Label();
-			this.LibraLabel = new System.Windows.Forms.Label();
 			this.CaDolLabel = new System.Windows.Forms.Label();
+			this.LibraLabel = new System.Windows.Forms.Label();
+			this.EuroLabel = new System.Windows.Forms.Label();
 			this.IeneLabel = new System.Windows.Forms.Label();
 			this.AusDolLabel = new System.Windows.Forms.Label();
+			this.currencyTimer = new System.Windows.Forms.Timer(this.components);
+			this.LoadingGif = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.LoadingGif)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// DollarLabel
 			// 
 			this.DollarLabel.BackColor = System.Drawing.Color.Transparent;
 			this.DollarLabel.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.DollarLabel.ForeColor = System.Drawing.Color.White;
 			this.DollarLabel.Location = new System.Drawing.Point(12, 12);
 			this.DollarLabel.Name = "DollarLabel";
 			this.DollarLabel.Size = new System.Drawing.Size(210, 50);
@@ -47,21 +52,23 @@ namespace CurrencyTracker
 			this.DollarLabel.Text = " $ 0,00";
 			this.DollarLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
-			// EuroLabel
+			// CaDolLabel
 			// 
-			this.EuroLabel.BackColor = System.Drawing.Color.Transparent;
-			this.EuroLabel.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.EuroLabel.Location = new System.Drawing.Point(12, 68);
-			this.EuroLabel.Name = "EuroLabel";
-			this.EuroLabel.Size = new System.Drawing.Size(210, 50);
-			this.EuroLabel.TabIndex = 1;
-			this.EuroLabel.Text = "C$ 0,00";
-			this.EuroLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.CaDolLabel.BackColor = System.Drawing.Color.Transparent;
+			this.CaDolLabel.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.CaDolLabel.ForeColor = System.Drawing.Color.White;
+			this.CaDolLabel.Location = new System.Drawing.Point(12, 68);
+			this.CaDolLabel.Name = "CaDolLabel";
+			this.CaDolLabel.Size = new System.Drawing.Size(210, 50);
+			this.CaDolLabel.TabIndex = 1;
+			this.CaDolLabel.Text = "C$ 0,00";
+			this.CaDolLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// LibraLabel
 			// 
 			this.LibraLabel.BackColor = System.Drawing.Color.Transparent;
 			this.LibraLabel.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.LibraLabel.ForeColor = System.Drawing.Color.White;
 			this.LibraLabel.Location = new System.Drawing.Point(12, 236);
 			this.LibraLabel.Name = "LibraLabel";
 			this.LibraLabel.Size = new System.Drawing.Size(210, 50);
@@ -69,21 +76,23 @@ namespace CurrencyTracker
 			this.LibraLabel.Text = " £ 0,00";
 			this.LibraLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
-			// CaDolLabel
+			// EuroLabel
 			// 
-			this.CaDolLabel.BackColor = System.Drawing.Color.Transparent;
-			this.CaDolLabel.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.CaDolLabel.Location = new System.Drawing.Point(12, 180);
-			this.CaDolLabel.Name = "CaDolLabel";
-			this.CaDolLabel.Size = new System.Drawing.Size(210, 50);
-			this.CaDolLabel.TabIndex = 3;
-			this.CaDolLabel.Text = " € 0,00";
-			this.CaDolLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.EuroLabel.BackColor = System.Drawing.Color.Transparent;
+			this.EuroLabel.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.EuroLabel.ForeColor = System.Drawing.Color.White;
+			this.EuroLabel.Location = new System.Drawing.Point(12, 180);
+			this.EuroLabel.Name = "EuroLabel";
+			this.EuroLabel.Size = new System.Drawing.Size(210, 50);
+			this.EuroLabel.TabIndex = 3;
+			this.EuroLabel.Text = " € 0,00";
+			this.EuroLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// IeneLabel
 			// 
 			this.IeneLabel.BackColor = System.Drawing.Color.Transparent;
 			this.IeneLabel.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.IeneLabel.ForeColor = System.Drawing.Color.White;
 			this.IeneLabel.Location = new System.Drawing.Point(12, 292);
 			this.IeneLabel.Name = "IeneLabel";
 			this.IeneLabel.Size = new System.Drawing.Size(210, 50);
@@ -95,6 +104,7 @@ namespace CurrencyTracker
 			// 
 			this.AusDolLabel.BackColor = System.Drawing.Color.Transparent;
 			this.AusDolLabel.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.AusDolLabel.ForeColor = System.Drawing.Color.White;
 			this.AusDolLabel.Location = new System.Drawing.Point(12, 124);
 			this.AusDolLabel.Name = "AusDolLabel";
 			this.AusDolLabel.Size = new System.Drawing.Size(210, 50);
@@ -102,27 +112,51 @@ namespace CurrencyTracker
 			this.AusDolLabel.Text = "A$ 0,00";
 			this.AusDolLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
+			// currencyTimer
+			// 
+			this.currencyTimer.Enabled = true;
+			this.currencyTimer.Interval = 60000;
+			this.currencyTimer.Tick += new System.EventHandler(this.currencyTimer_Tick);
+			// 
+			// LoadingGif
+			// 
+			this.LoadingGif.BackColor = System.Drawing.Color.Transparent;
+			this.LoadingGif.Image = global::CurrencyTracker.Properties.Resources.loading;
+			this.LoadingGif.InitialImage = null;
+			this.LoadingGif.Location = new System.Drawing.Point(12, 20);
+			this.LoadingGif.Name = "LoadingGif";
+			this.LoadingGif.Size = new System.Drawing.Size(30, 30);
+			this.LoadingGif.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.LoadingGif.TabIndex = 6;
+			this.LoadingGif.TabStop = false;
+			this.LoadingGif.Visible = false;
+			// 
 			// CurrencyTeller
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(7)))), ((int)(((byte)(27)))));
 			this.ClientSize = new System.Drawing.Size(234, 361);
+			this.Controls.Add(this.LoadingGif);
 			this.Controls.Add(this.AusDolLabel);
 			this.Controls.Add(this.IeneLabel);
-			this.Controls.Add(this.CaDolLabel);
-			this.Controls.Add(this.LibraLabel);
 			this.Controls.Add(this.EuroLabel);
+			this.Controls.Add(this.LibraLabel);
+			this.Controls.Add(this.CaDolLabel);
 			this.Controls.Add(this.DollarLabel);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Location = new System.Drawing.Point(2566, 132);
 			this.MaximizeBox = false;
 			this.MaximumSize = new System.Drawing.Size(250, 400);
 			this.MinimizeBox = false;
 			this.MinimumSize = new System.Drawing.Size(250, 400);
 			this.Name = "CurrencyTeller";
+			this.Opacity = 0.8D;
 			this.ShowIcon = false;
-			this.Text = "Currency Teller";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+			this.Text = "Currency Tracker";
 			this.Load += new System.EventHandler(this.Form1_Load);
-			this.Shown += new System.EventHandler(this.Form1_Shown);
+			((System.ComponentModel.ISupportInitialize)(this.LoadingGif)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -130,11 +164,13 @@ namespace CurrencyTracker
 		#endregion
 
 		private System.Windows.Forms.Label DollarLabel;
-		private System.Windows.Forms.Label EuroLabel;
-		private System.Windows.Forms.Label LibraLabel;
 		private System.Windows.Forms.Label CaDolLabel;
+		private System.Windows.Forms.Label LibraLabel;
+		private System.Windows.Forms.Label EuroLabel;
 		private System.Windows.Forms.Label IeneLabel;
 		private System.Windows.Forms.Label AusDolLabel;
+		private System.Windows.Forms.Timer currencyTimer;
+		private System.Windows.Forms.PictureBox LoadingGif;
 	}
 }
 
--- CurrencyTeller/CurrencyTeller.cs
+++ CurrencyTeller/CurrencyTeller.cs
@@ -1,81 +1,109 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Net;
-using System.IO;
 using System.Globalization;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
 
 // Dollar html location: <div class="BNeawe iBp4i AP7Wnd"><div><div class="BNeawe iBp4i AP7Wnd">
 namespace CurrencyTracker
 {
 	public partial class CurrencyTeller : Form
 	{
-		string location = "<div class=\"BNeawe iBp4i AP7Wnd\"><div><div class=\"BNeawe iBp4i AP7Wnd\">";
+		WebClient client = new WebClient();
 
-		float dollar = 0.00f;
-		float caDol = 0.00f;
-		float ausDol = 0.00f;
-		float euro = 0.00f;
-		float libra = 0.00f;
-		float iene = 0.00f;
+		string location = "<div class=\"BNeawe iBp4i AP7Wnd\"><div><div class=\"BNeawe iBp4i AP7Wnd\">";
+		string googlePage = "https://www.google.com/search?q=";
+
+		string[] currencies = new string[] { "dólar", "dólar+canadense", "dólar+australiano", "euro", "libra+real", "iene" };
+		string[] currencySigns = new string[] { "$", "C$", "A$", "€", "£", "¥" };
+		Label[] labels;
 
 		public CurrencyTeller()
 		{
 			InitializeComponent();
+			SendToBack();
 		}
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
-			DollarLabel.Text = " $ " + dollar;
-			CaDolLabel.Text = "C$ " + caDol;
-			AusDolLabel.Text = "A$ " + ausDol;
-			EuroLabel.Text = " € " + euro;
-			LibraLabel.Text = " £ " + libra;
-			IeneLabel.Text = " ¥ " + iene;
+			client.Encoding = Encoding.UTF8;
+			labels = new Label[] { DollarLabel, CaDolLabel, AusDolLabel, EuroLabel, LibraLabel, IeneLabel };
+
+			Thread t = new Thread(new ThreadStart(UpdateCurrency));
+			t.Start();
+
+			SendToBack();
 		}
 
-		private void Form1_Shown(object sender, EventArgs e)
+		private void currencyTimer_Tick(object sender, EventArgs e)
 		{
-			// START CLIENT
-			WebClient client = new WebClient();
-			client.Encoding = Encoding.UTF8;
+			Thread t = new Thread(new ThreadStart(UpdateCurrency));
+			t.Start();
 
-			// GET GOOGLE CURRENCY HTML
-			string downloadedStringDollar = client.DownloadString("https://www.google.com/search?q=dólar");
-			string downloadedStringCaDol = client.DownloadString("https://www.google.com/search?q=dólar+canadense");
-			string downloadedStringAusDol = client.DownloadString("https://www.google.com/search?q=dólar+australiano");
-			string downloadedStringEuro = client.DownloadString("https://www.google.com/search?q=euro");
-			string downloadedStringLibra = client.DownloadString("https://www.google.com/search?q=libra+real");
-			string downloadedStringIene = client.DownloadString("https://www.google.com/search?q=iene");
+			SendToBack();
+		}
 
-			// GET DOLLAR INDEX AND THE CURRENCY VALUE
-			string dollarValue = downloadedStringDollar.Substring(downloadedStringDollar.IndexOf(location) + 71, 4);
-			string caDolValue = downloadedStringCaDol.Substring(downloadedStringCaDol.IndexOf(location) + 71, 4);
-			string ausDolValue = downloadedStringAusDol.Substring(downloadedStringAusDol.IndexOf(location) + 71, 4);
-			string euroValue = downloadedStringEuro.Substring(downloadedStringEuro.IndexOf(location) + 71, 4);
-			string libraValue = downloadedStringLibra.Substring(downloadedStringLibra.IndexOf(location) + 71, 4);
-			string ieneValue = downloadedStringIene.Substring(downloadedStringIene.IndexOf(location) + 71, 4);
+		void UpdateCurrency()
+		{
+			for (int i = 0; i < currencies.Length; i++)
+			{
+				SetLoading(true);
+				SetLoadingLocation(i);
 
-			dollar = float.Parse(dollarValue, CultureInfo.InvariantCulture);
-			caDol = float.Parse(dollarValue, CultureInfo.InvariantCulture);
-			ausDol = float.Parse(ausDolValue, CultureInfo.InvariantCulture);
-			euro = float.Parse(euroValue, CultureInfo.InvariantCulture);
-			libra = float.Parse(libraValue, CultureInfo.InvariantCulture);
-			iene = float.Parse(ieneValue, CultureInfo.InvariantCulture);
+				string downloadedString = client.DownloadString(googlePage + currencies[i]);
 
-			// INPUT VALUE INTO FORMS
-			DollarLabel.Text =	" $ " + dollar;
-			CaDolLabel.Text =	"C$ " + caDol;
-			AusDolLabel.Text =	"A$ " + ausDol;
-			EuroLabel.Text =	" € " + euro;
-			LibraLabel.Text =	" £ " + libra;
-			IeneLabel.Text =	" ¥ " + iene;
+				float currency = float.Parse(downloadedString.Substring(downloadedString.IndexOf(location) + 71, 4), CultureInfo.InvariantCulture);
+				SetText(currencySigns[i], " " + currency, i);
+
+				Thread.Sleep(0);
+
+				while (client.IsBusy) { }
+			}
+			SetLoading(false);
+		}
+
+		delegate void SetTextCallback(string currencySign, string text, int i);
+		private void SetText(string currencySign, string text, int i)
+		{
+			if (labels[i].InvokeRequired)
+			{
+				SetTextCallback d = new SetTextCallback(SetText);
+				Invoke(d, new object[] { currencySign, text, i });
+			}
+			else
+			{
+				if (currencySign.Length > 1)
+					labels[i].Text = currencySign + text;
+				else
+					labels[i].Text = " " + currencySign + text;
+			}
+		}
+		delegate void SetLoadingLocationCallback(int i);
+		private void SetLoadingLocation(int i)
+		{
+			if (labels[i].InvokeRequired)
+			{
+				SetLoadingLocationCallback d = new SetLoadingLocationCallback(SetLoadingLocation);
+				Invoke(d, new object[] { i });
+			}
+			else
+			{
+				LoadingGif.Location = new System.Drawing.Point(12, labels[i].Location.Y + 8);
+			}
+		}
+		delegate void SetLoadingCallback(bool visible);
+		void SetLoading(bool visible)
+		{
+			if (LoadingGif.InvokeRequired)
+			{
+				SetLoadingCallback d = new SetLoadingCallback(SetLoading);
+				Invoke(d, new object[] { visible });
+			}
+			else
+			{
+				LoadingGif.Visible = visible;
+			}
 		}
 	}
 }
